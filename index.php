973d3fb14d7e24f91e49f3534645ab6f1891de74
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-07-03 17:28:41 +0200

    aggiunta prime funzioni

--- index.php
+++ index.php
@@ -18,4 +18,43 @@ class Stock {
 
 }
 
-public function __construct( $name, $cost, $quantity, $score, )
+public function getName() {
+
+    return $this -> name;
+
+}
+public function setName( $name ) {
+
+    $this -> name;
+
+}
+public function getCost() {
+
+    return $this -> cost;
+
+}
+public function setCost( $cost ) {
+
+    $this -> cost;
+    
+}
+public function getQuantity() {
+
+    return $this -> quantity;
+
+}
+public function setQuantity( $quantity ) {
+
+    $this -> quatity;
+    
+}
+public function getScore() {
+
+    return $this -> score;
+
+}
+public function setScore( $score ) {
+
+    $this -> score;
+    
+}
